14c9c20b5079f959f09376cfb518161d2fa08df2
Anonymous <noreply@weblate.org>
2023-11-02 07:56:32 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: PalliativeTris/Backoffice
    Translate-URL: http://localhost/projects/palliativetris/backoffice/en/

--- lang/en/texts.php
+++ lang/en/texts.php
@@ -15,5 +15,6 @@ return [
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
+    'reset2' => 'reset2',
 ];
 $pippo = 'franco';
